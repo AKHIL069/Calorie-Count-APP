8ed8f1053456c2a1c1fe19af3f49b1d24ea3a6e2
akhil <dev-016@icingtree.com>
2025-07-16 12:39:08 +0530

    add route to app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Register from './components/UserRegister'
+import Login from './components/UserLogin';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   )
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
-import Register from './components/UserRegister'
-import Login from './components/UserLogin';
-import Dashboard from './components/Dashboard';
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+   <App />
   </React.StrictMode>
 );
